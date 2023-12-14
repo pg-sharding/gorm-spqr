7fd2ced3ac8938a17d5c8a3e2599fe1a4bb864ef
Yury Frolov <yura200253@gmail.com>
2023-12-14 16:40:54 +0500

    Added extra parameters in DSN via env

--- models/setup.go
+++ models/setup.go
@@ -11,12 +11,13 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s TimeZone=UTC",
+		"host=%s user=%s password=%s dbname=%s port=%s TimeZone=UTC %s",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
+		os.Getenv("EXTRA_PARAMS"),
 	)
 	database, err := gorm.Open(postgres.New(postgres.Config{
 		DSN:                  dsn,
