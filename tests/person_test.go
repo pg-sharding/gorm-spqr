2b75c7b462447bdedb8548e37efedebd754402a2
Yury Frolov <yura200253@gmail.com>
2024-01-16 11:34:47 +0500

    Added test for writing multiple people

--- tests/person_test.go
+++ tests/person_test.go
@@ -1,9 +1,11 @@
 package tests
 
 import (
+	"fmt"
 	"github.com/pg-sharding/gorm-spqr/controllers"
 	"github.com/pg-sharding/gorm-spqr/models"
 	"github.com/pg-sharding/gorm-spqr/utils"
+	"github.com/stretchr/testify/assert"
 	"os"
 	"reflect"
 	"testing"
@@ -131,4 +133,48 @@ func TestDeletePerson(t *testing.T) {
 	}
 
 	tearDown(t)
+}
+
+func TestMultipleWrite(t *testing.T) {
+	setup()
+
+	var firstShard = make([]*models.Person, 0)
+	for i := 1; i < 100; i++ {
+		person := &models.Person{
+			ID:        uint32(i),
+			FirstName: fmt.Sprintf("Name_%d", i),
+		}
+		firstShard = append(firstShard, person)
+	}
+
+	if err := controllers.WritePeople(firstShard); err != nil {
+		t.Errorf("could not write: %s", err)
+	}
+
+	var secondShard = make([]*models.Person, 0)
+	for i := 100; i < 200; i++ {
+		person := &models.Person{
+			ID:        uint32(i),
+			FirstName: fmt.Sprintf("Name_%d", i),
+		}
+		secondShard = append(secondShard, person)
+	}
+
+	if err := controllers.WritePeople(secondShard); err != nil {
+		t.Errorf("could not write: %s", err)
+	}
+
+	firstShardPeople, err := controllers.GetPeople(uint32(1), uint32(99))
+	if err != nil {
+		t.Errorf("could not get people: %s", err)
+	}
+
+	assert.Equalf(t, len(firstShardPeople), 99, "expected to have %d records on 1st shard, got %d", 99, len(firstShardPeople))
+
+	secondShardPeople, err := controllers.GetPeople(uint32(100), uint32(199))
+	if err != nil {
+		t.Errorf("could not get people: %s", err)
+	}
+
+	assert.Equalf(t, len(secondShardPeople), 100, "expected to have %d records on 1st shard, got %d", 100, len(secondShardPeople))
 }
